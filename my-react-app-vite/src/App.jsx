3912bb39e5132e3a2331fa31bb8584578e22bb02
SubhamKumar101201 <das.subham101201@gmail.com>
2024-01-05 11:50:54 +0530

    theme switcher using useContext

--- my-react-app-vite/src/App.jsx
+++ my-react-app-vite/src/App.jsx
@@ -1,14 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import UserContextProvider from './context/UserContextProvider';
-import Login from './components/MiniContext/Login';
-import Profile from './components/MiniContext/Profile';
+import { ThemeProvider } from './context/Theme';
+import Theme from './components/ThemeSwitcher/Theme';
+import Card from './components/ThemeSwitcher/Card';
 
 const App = () => {
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
   return (
-        <UserContextProvider>
-          <Profile/>
-          <Login/>
-        </UserContextProvider>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+
+      <div className="flex flex-wrap mt-4 min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Theme />
+          </div>
+
+          <div className="flex flex-col gap-y-2 w-full max-w-sm mx-auto">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+      </div>
+
+    </ThemeProvider>
   );
 };
 
